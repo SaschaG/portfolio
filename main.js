7c3547703fa1a6be66e4a3ce35cbd810badea9bb
SaschaG <sushi.genji@hotmail.de>
2017-02-26 14:19:43 +0100

    fixed the main bar and added first content

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
 $(function(){
 
-	$(".mainbar").css("height", $(window).height()-30);
+	var adjustToWindowHeight = function() {
+		
+		console.log("Window Height " + window.height + "Window Outer Height " + window.outerHeight  + "inner Height " + window.innerHeight)
+
+		if(window.innerHeight > 550 && window.outerWidth > 992){
+			$(".mainbar").css("height", $(window).height()-30);
+
+			if(window.innerHeight <= 885){
+				$("#second-p").hide();
+			}
+			else{
+				$("#second-p").show();
+			}
+
+			if(window.innerHeight < 680){
+				$("#first-p").hide();
+			}
+			else{
+				$("#first-p").show();
+			}
+		}
+
+		if(window.innerHeight > 550 && window.outerWidth <= 992){
+			$("#second-p").show();
+			$("#first-p").show();
+			$(".mainbar").css("height", "auto");
+		}
+	}
+
+	
+
+
+	$(window).resize(function(){
+
+		adjustToWindowHeight();
+	});
+
+
+	adjustToWindowHeight(); 
 	console.log("JQuerry Loaded and rdy to use");
 });
